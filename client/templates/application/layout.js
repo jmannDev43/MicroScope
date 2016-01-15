ac93837175a0a4463317e0e5eba64131746fae42
Josh Mann <jmann5443@gmail.com>
2016-01-14 22:15:33 -0600

    finished chappter 14 (animations)

--- client/templates/application/layout.js
+++ client/templates/application/layout.js
@@ -1,3 +1,16 @@
 Template.layout.helpers({
    pageTitle: function() { return Session.get('pageTitle') || 'MicroScope' }
 });
+
+Template.layout.onRendered(function() {
+   this.find('#main')._uihooks = {
+       insertElement: function(node, next) {
+           $(node).hide().insertBefore(next).fadeIn();
+       },
+       removeElement: function(node) {
+          $(node).fadeOut(function() {
+              $(this).remove();
+          });
+       }
+   }
+});
